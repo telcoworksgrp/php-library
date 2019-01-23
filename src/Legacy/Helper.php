6d94e5a1dc8f8631b10efc6b2162ec9df3ab5e4b
David Plath <webmaster@telecomcorp.com.au>
2019-01-23 14:58:39 +1100

    Added method for sending HTTP requests

--- src/Legacy/Helper.php
+++ src/Legacy/Helper.php
@@ -93,4 +93,25 @@ class Helper
         return self::isT2AffilateReferral OR self::isT3AffilateReferral;
     }
 
-}
+
+
+    /**
+     * Send a HTTP request return the result
+     * -------------------------------------------------------------------------
+     * @param  string $url      URL to send the request to
+     * @param  string $method   HTTP method to use
+     * @param  array  $data     Data to send with request
+     * @return string           The HTTP reponse body
+     */
+    function sendRequest($url, $method = 'GET', $data = array(),
+        $headers = array('Content-type: application/x-www-form-urlencoded') )
+        {
+            $context = stream_context_create(array('http' => array(
+                    'method' => $method,
+                    'header' => $headers,
+                    'content' => http_build_query($data)
+            )));
+
+            return file_get_contents($url, false, $context);
+        }
+    }
