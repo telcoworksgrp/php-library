0eb68d8486fa9c2bc7eaea49077fef1f6b580f2e
David Plath <webmaster@telecomcorp.com.au>
2019-02-14 10:17:03 +1100

    Added template, plugin and library helper classes

---
+++ src/Joomla/Template/Helper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace TCorp\Joomla\Template;
+
+defined('_JEXEC') or die();
+
+class Helper
+{
+}
