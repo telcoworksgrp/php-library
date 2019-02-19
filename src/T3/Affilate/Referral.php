925066328d345e4d5d9ce35d69fa96fe53d51712
David Plath <webmaster@telecomcorp.com.au>
2019-02-19 15:02:12 +1100

    Moved affilate referral code into a seperate class

---
+++ src/T3/Affilate/Referral.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace TCorp\T3\Affilate;
+
+
+/**
+ * Helper class for working with T3 affilate referrals from other sites
+ */
+class Refferal
+{
+
+    /**
+     * The name of the URL parameter, which may exist - and if so, is expected
+     * to contain an affilate id
+     *
+     * @var string
+     */
+    public static $urlParam = 'affilate';
+
+
+    /**
+     * The name of the cookie, which is responsiable
+     * for storing the affilate id across page requests
+     *
+     * @var string
+     */
+    public static $cookieName = 'affilate';
+
+
+
+    /**
+     * Detect a new affilate referral and then update
+     * -------------------------------------------------------------------------
+     * @return  void
+     */
+    public static function detect()
+    {
+        if (isset($_GET[self::$urlParam])) {
+            self::setId($_GET[self::$urlParam]);
+        }
+    }
+
+
+    /**
+     * Gets the current affilate id
+     * -------------------------------------------------------------------------
+     * @return mixed   The current affilate id, or FALSE in no referal has
+     *                 been detected
+     */
+    public static function getId()
+    {
+        return (isset($_COOKIE[self::$cookieName])) ?
+            $_COOKIE[self::$cookieName] : FALSE;
+    }
+
+
+    /**
+     * Manually set/update the affilate id
+     * -------------------------------------------------------------------------
+     * @param   string    $id         A new affilate id
+     * @param   bool      $validate   Check that the given value is valid
+     * @return  void
+     */
+    public static function setId(string $id, bool $validate = TRUE)
+    {
+        if ($validate AND self::validateId($id)) {
+
+            setrawcookie(self::$cookieName, $id);
+            $_COOKIE[self::$cookieName] = $id;
+
+        }
+    }
+
+
+    /**
+     * Validate an affilate id. Valid T3 affilate ids consist of 12 uppercase
+     * letters (new signups in T3); or 8 characters including lower case
+     * letters and digits (T2 customers migrated to T3)
+     * -------------------------------------------------------------------------
+     * @return  bool   TRUE if the given id is valid, FALSE if not
+     */
+    protected static function validateId(string $id)
+    {
+        $result = preg_match('|^[A-Z]{12}$|', $id)
+            OR preg_match('|^[A-Za-z0-9]{8}$|', $id);
+
+        return (bool) $result;
+    }
+
+
+    /**
+     * Determins if an affilate reffral has previously been detected
+     * -------------------------------------------------------------------------
+     * @return  bool TRUE if an affilate reffral has been detected, FALSE if not
+     */
+    public static function isReferral()
+    {
+        return self::getId() !== FALSE;
+    }
+
+
+    /**
+     * Clear any existing affilate referal
+     * -------------------------------------------------------------------------
+     * @return  void
+     */
+    public static function clear()
+    {
+        setcookie(self::$cookieName, "", time()-3600);
+        if (isset($_COOKIE[self::$cookieName])) {
+            unset($_COOKIE[self::$cookieName]);
+        }
+
+    }
+
+
+}
--- src/T3/Helper.php
+++ src/T3/Helper.php
@@ -7,71 +7,10 @@
 
 namespace TCorp\T3;
 
-use \TCorp\Legacy\Helper AS LegacyHelper;
-
 
 /**
  * Helper class for working with Telecom Corp's T3 platform
  */
 class Helper
 {
-
-
-    /**
-     * Detects a T3 affilate refferal and sets the appropriate cookies so that
-     * the affilate id is remebered across subsequent requests
-     * -------------------------------------------------------------------------
-     */
-    public static function detectAffilateRefferal()
-    {
-        // If an "affilate" URL param exists, and it's value is a valid
-        // affilate id then set an "affilate" cookie to remember that value
-        // across subsequent requests
-        if (isset($_GET['affilate']) AND self::validateAffilateId(
-            $_GET['affilate'])) {
-
-            setrawcookie('affilate', $_GET['affilate']);
-        }
-    }
-
-
-    /**
-     * Get the affilate id that has been last detected
-     * -------------------------------------------------------------------------
-     * @return string|bool  A affilate id on success, FALSE if none is found
-     */
-    public static function getAffilateRefferalId()
-    {
-        // Initialise some local variables
-        $result = FALSE;
-
-        // If an "affilate" URL param exits OR an "affilate" cookie has
-        // previously been set - and in any case the value is valid -
-        // then consider that value as the current affilate id
-        if (isset($_REQUEST['affilate']) AND
-            self::validateAffilateId($_REQUEST['affilate'])) {
-
-            $result = $_REQUEST['affilate'];
-        }
-
-        // Return the result
-        return $result;
-    }
-
-
-    /**
-     * Check if a given T3 affilate id is valid. Valid T3 affilate ids consist
-     * of 12 uppercase letters (new signups in T3); or 8 characters including
-     * lower case letters and digits (T2 customers migrated to T3)
-     * -------------------------------------------------------------------------
-     * @param  string   $id     An affilate id to check
-     * @return bool             TRUE is valid, FALSE is invalid
-     */
-    protected static function validateAffilateId(string $id) : bool
-    {
-        return (bool) preg_match('|^[A-Z]{12}$|', $id) OR
-            LegacyHelper::validateT2AffilateId($id);
-    }
-
-
 }
