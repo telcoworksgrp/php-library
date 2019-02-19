94efc5306d6323682e54d783d2e4e4f2dada6bae
David Plath <webmaster@telecomcorp.com.au>
2019-02-19 14:46:55 +1100

    Added class for working with Google Analytics

---
+++ src/Google/Analytics.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace TCorp\Google;
+
+/**
+ * Class for working with Google Analytics
+ */
+class Analytics
+{
+
+    /**
+     * Get Google's tracking code snippit.
+     * -------------------------------------------------------------------------
+     * @param  string   $tackingId  A tracking id obtained from Google Analytics
+     * @return string   Tracking code snippet
+     */
+    public static function getTrackingCode(string $tackingId)
+    {
+        $result = "<script async src=""https://www.googletagmanager.com/gtag/" .
+            "js?id=$trackingId""></script><script>window.dataLayer = window.".
+            "dataLayer || []; function gtag(){dataLayer.push(arguments);} ".
+            "gtag('js',new Date()); gtag('config','$trackingId'); </script>";
+
+    }
+
+}
