89e1ad013b6d1d83dcd700d5f97696444c660c24
David Plath <webmaster@telecomcorp.com.au>
2019-02-21 15:50:25 +1100

    Added helper class for working with dates in Joomla

---
+++ src/Joomla/Date/Helper.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace TCorp\Joomla\Date;
+
+use \Joomla\CMS\HTML\HTMLHelper;
+
+
+/**
+ * Helper class for working with dates
+ */
+class Helper
+{
+
+    /**
+     * Format a MySQL datetime value into a more user friendly format
+     * -------------------------------------------------------------------------
+     * @param  string   $dateTime   The MySQL datetime value to format
+     * @param  string   $format     The output format (optional)
+     * @param  string   $default    Default value if date is '0'/invalid
+     *
+     * @return string   A more user friendly datetime string
+     */
+    public static function formatMysqlDateTime(string $dateTime, string
+        $format = 'd/m/Y h:i:s A', string $default = '-')
+    {
+        // If the value is '0' or invalid the return the default
+        if (empty($dateTime) OR ($dateTime == '0000-00-00 00:00:00')) {
+            return $default;
+        }
+
+        // Get and return the new value
+        return HTMLHelper::_('date', $dateTime, $format, true);
+    }
+
+}
