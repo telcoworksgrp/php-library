fe4a6d4f28351554cd85a804c00e8b4bb6b66406
David Plath <webmaster@telecomcorp.com.au>
2019-03-01 11:05:33 +1100

    Added helper for working with the document in Joomla

---
+++ src/Joomla/Document/DocumentHelper.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace TCorp\Joomla\Document;
+
+
+use \Joomla\CMS\Factory;
+
+
+/**
+ * Helper class containing miscellanious methods for working with
+ * Joomla documents
+ */
+class DocumentHelper
+{
+
+
+    /**
+     * Proxy for adding an external script to the document, except a
+     * little easier/cleaner when SRI hashes need to be added.
+     * -------------------------------------------------------------------------
+     * @param string    $url           URL to the eternal javascript
+     * @param string    $integrity     An SRI hash
+     *
+     * @return  void
+     */
+    public static function addScript(string $url, string $integrity = '')
+    {
+        // Initialise some local variables
+        $options    = array();
+        $attributes = array();
+
+        // Add SRI hash if provoided
+        if (!empty($integrity)) {
+            $attributes['integrity']   = $integrity;
+            $attributes['crossorigin'] = 'anonymous';
+        }
+
+        // Add the script to the document
+        Factory::getDocument()->addScript($url, $options, $attributes);
+    }
+    
+
+
+    /**
+     * Proxy for adding an external stylesheets to the document, except a
+     * little easier/cleaner when SRI hashes need to be added.
+     * -------------------------------------------------------------------------
+     * @param string    $url           URL to the eternal stylesheet
+     * @param string    $integrity     An SRI hash
+     *
+     * @return  void
+     */
+    public static function addStylesheet(string $url, string $integrity = '')
+    {
+        // Initialise some local variables
+        $options    = array();
+        $attributes = array();
+
+        // Add SRI hash if provoided
+        if (!empty($integrity)) {
+            $attributes['integrity']   = $integrity;
+            $attributes['crossorigin'] = 'anonymous';
+        }
+
+        // Add the script to the document
+        Factory::getDocument()->addStylesheet($url, $options, $attributes);
+    }
+
+
+
+}
