a3fe034e067ac78a723635b4b42b26865188d144
David Plath <webmaster@telecomcorp.com.au>
2019-03-18 13:57:06 +1100

    Removed unnecessary methods from Joomla model base classes

--- src/Joomla/Model/Admin/Generic.php
+++ src/Joomla/Model/Admin/Generic.php
@@ -13,24 +13,4 @@ use \Joomla\CMS\MVC\Model\BaseDatabaseModel;
 
 class Generic extends BaseDatabaseModel
 {
-
-    /**
-     * Get a table object, load it if necessary.
-     * -------------------------------------------------------------------------
-     * @param  string   $type       The table name.
-     * @param  string   $prefix     The class prefix.
-     * @param  array    $config     Configuration array for table.
-     *
-     * @return \JTable              A JTable object
-     */
-    public function getTable($type = '', $prefix = '', $config = array())
-    {
-        // If not given, detect type and prefix based on the class name
-        $type   = (empty($type)) ? $this->getName() : $type;
-        $prefix = (empty($prefix)) ? ucfirst($this->option) . 'Table' : $prefix;
-
-        // Call and return the parent method
-        return parent::getTable($type, $prefix, $config);
-    }
-
 }
