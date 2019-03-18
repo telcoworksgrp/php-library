d87dfd0902bcc481adba54d9bf9bb5e0753900f2
David Plath <webmaster@telecomcorp.com.au>
2019-03-18 14:01:31 +1100

    Added a method override for auto populating the item model with the item id

--- src/Joomla/Model/Site/Item.php
+++ src/Joomla/Model/Site/Item.php
@@ -13,4 +13,23 @@ use \Joomla\CMS\MVC\Model\ItemModel;
 
 class Item extends ItemModel
 {
+
+    /**
+     * Auto-populate the model state.
+     * -------------------------------------------------------------------------
+     * @return  void
+     */
+    protected function populateState()
+    {
+        // Initialize some local variables
+        $application = Factory::getApplication();
+        $input       = $application->input;
+
+        // Set the item id in the model state
+		$this->setState('id', $input->getInt('id', 0));
+
+        // Call the parent method
+        parent::populateState();
+    }
+
 }
