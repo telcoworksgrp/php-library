2c0fdeca3ea2440f204f62fa4d530e470efbb04d
David Plath <webmaster@telecomcorp.com.au>
2019-05-17 16:56:38 +1000

    Added methods to toolbar helper for adding buttons

--- src/Joomla/Toolbar/ToolbarHelper.php
+++ src/Joomla/Toolbar/ToolbarHelper.php
@@ -32,4 +32,77 @@ class ToolbarHelper
         JoomlaToolBarHelper::title(Text::_($title));
     }
 
+
+    /**
+     * Add a create/new button to the toolbar
+     * -------------------------------------------------------------------------
+     * @param   string    $component     The component the controller belongs to
+     * @param   string    $controller    The sub-controller to run
+     */
+    public static function addCreateBtn(string $component, string $controller)
+    {
+        if (UserHelper::isAuthorised('core.create', $component)) {
+            JoomlaToolBarHelper::addNew($controller . '.add');
+        }
+    }
+
+
+    /**
+     * Add a edit button to the toolbar
+     * -------------------------------------------------------------------------
+     * @param   string    $component     The component the controller belongs to
+     * @param   string    $controller    The sub-controller to run
+     */
+    public static function addCreateBtn(string $component, string $controller)
+    {
+        if (UserHelper::isAuthorised('core.edit', $component)) {
+            JoomlaToolBarHelper::editList($controller . '.add');
+        }
+    }
+
+
+    /**
+     * Add a publich and unpublish button to the toolbar
+     * -------------------------------------------------------------------------
+     * @param   string    $component     The component the controller belongs to
+     * @param   string    $controller    The sub-controller to run
+     */
+    public static function addPublishBtns(string $component, string $controller)
+    {
+        if (UserHelper::isAuthorised('core.edit.state', $component)) {
+            JoomlaToolBarHelper::publish($controller . '.publish',
+                'JTOOLBAR_PUBLISH', true);
+            JoomlaToolBarHelper::unpublish($controller . '.unpublish',
+                'JTOOLBAR_UNPUBLISH', true);
+        }
+    }
+
+
+    /**
+     * Add a delete button to the toolbar
+     * -------------------------------------------------------------------------
+     * @param   string    $component     The component the controller belongs to
+     * @param   string    $controller    The sub-controller to run
+     */
+    public static function addDeleteBtn(string $component, string $controller)
+    {
+        if (UserHelper::isAuthorised('core.delete', $component)) {
+            JoomlaToolBarHelper::deleteList('', $listController . '.delete');
+        }
+    }
+
+
+    /**
+     * Add a component options button to the toolbar
+     * -------------------------------------------------------------------------
+     * @param   string    $component     The component name
+     */
+    public static function addOptionsBtn(string $component)
+    {
+        if (UserHelper::isAuthorised('core.admin', $component)) {
+            JoomlaToolBarHelper::preferences($component);
+        }
+    }
+
+
 }
