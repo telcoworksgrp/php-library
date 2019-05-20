824b992af2e110d9b43c2022badf2e6bdd9d90a6
David Plath <webmaster@telecomcorp.com.au>
2019-05-20 10:17:47 +1000

    Moved joomla user helper methods back to the generic joomla helper

--- src/Joomla/Helper/JoomlaHelper.php
+++ src/Joomla/Helper/JoomlaHelper.php
@@ -250,8 +250,38 @@ class JoomlaHelper
     public static function getActiveMenuItem()
     {
         return Factory::getApplication()->getMenu()->getActive();
-    } 
+    }
 
 
+    /**
+     * Check if the a user is authorised to perform an action on a given asset
+     * -------------------------------------------------------------------------
+     * @param  string   $action     The action requested to perform
+     * @param  string   $asset      The asset on which the action to be performed
+     * @param  mixed    $userId     Id of the user, or null for the current user
+     *
+     * @return bool     TRUE if authorised, FALSE is not authorised
+     */
+    public static function isAuthorised(string $action, string $asset, $userId = null)
+    {
+        // Check if the user is authorised to perform the action on
+        // the given asset and return the result
+        return Factory::getUser($userId)->authorise($action, $asset);
+    }
 
+
+    /**
+     * Get the full name (as opposed to username) of given user id
+     * -------------------------------------------------------------------------
+     * @param   int     $userId     The id of the user
+     * @param   string  $default    A value to return if unsucessful
+     *
+     * @return  string The full name of the given user
+     */
+    public static function getFullName($userId = 0, string $default = '-')
+    {
+        // Get and return the user's name
+        return (empty($userId)) ? $default : Factory::getUser($userId)->name;
+    }
+    
 }
