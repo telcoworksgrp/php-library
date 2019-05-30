8ed4b4527734c07ad09e3ea3100e91febf8e499c
David Plath <webmaster@telecomcorp.com.au>
2019-05-30 10:28:43 +1000

    Added various method to legacy helper for managing session variables

--- src/Legacy/LegacyHelper.php
+++ src/Legacy/LegacyHelper.php
@@ -22,14 +22,6 @@ class LegacyHelper
 {
 
     /**
-     * An array for holding a list of settings
-     *
-     * @var string[]
-     */
-    public static $SETTINGS = array();
-
-
-    /**
      * An API key issued by IP Geolocation
      *
      * @var string
@@ -337,19 +329,6 @@ class LegacyHelper
 
 
     /**
-     * Start the user's session if not already started
-     * -------------------------------------------------------------------------
-     * @return void
-     */
-    public static function startSession()
-    {
-        if (!isset($_SESSION)) {
-            session_start();
-        }
-    }
-
-
-    /**
      * Try to detect any affilate id's passed via the https request. If one
      * is found,then store the id in the user's session.
      * -------------------------------------------------------------------------
@@ -367,6 +346,92 @@ class LegacyHelper
             return false;
         }
     }
+
+
+    /**
+     * Start the user's session if not already started
+     * -------------------------------------------------------------------------
+     * @return void
+     */
+    public static function startSession()
+    {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+    }
+
+    /**
+     * Set a value in the user's session
+     * -------------------------------------------------------------------------
+     * @param string    $key    A key name for referancing the stored value
+     * @param mixed     $value  The value to store
+     */
+    public static function setSessionVar(string $key, $value)
+    {
+        // Set a session variable with the given value
+        $_SESSION[$key] = $value;
+    }
+
+
+    /**
+     * Get a value previously stored in the user's session. If a value with
+     * the given key can not be found then a default can be returned
+     * -------------------------------------------------------------------------
+     * @param  string $key      A key name for referancing the stored value
+     * @param  mixed  $default  A default value if the key doesn't exist
+     *
+     * @return mixed    A value for the given key, or the default value
+     */
+    public static function getSessionVar(string $key, $default = null) : mixed
+    {
+        return $_SESSION[$key] ?? $default;
+    }
+
+
+    /**
+     * Unsets/removes an existing session variable
+     * -------------------------------------------------------------------------
+     * @param string    $key    A key name for referancing the stored value
+     *
+     * @return  mixed   The former value of the session var
+     */
+    public static function unsetSessionVar(string $key)
+    {
+        $result = self::getSessionVar($key);
+        unset($_SESSION[$key]);
+        return $result;
+    }
+
+
+    /**
+     * Store the value of a request variable in a session var. If the request
+     * var doesn't exist then preserve the existing session var. If a session
+     * var with the given key doesn't exist then set a session var with the
+     * given key to a given default value.
+     * -------------------------------------------------------------------------
+     * @param string    $key        A key name for referancing the stored value
+     * @param string    $var        A GET/POST variable name
+     * @param string    $default    Default value if both request and session
+     *                              var doesn't exist
+     * @param string    $filter     Filter Type (for sanitisation)
+     *
+     * @return  mixed   The final value of session var
+     */
+    public static function setSessionVarFromRequest(string $key, string $var,
+        $default = '', string $filter = 'STRING')
+    {
+        if (isset($_REQUEST[$var])) {
+            self::setSessionVar($key, $_REQUEST[$var]);
+        } else {
+            if (!isset($_SESSION[$key])) {
+                self::setSessionVar($key, $default);
+            }
+        }
+
+        // Return the result
+        return self::getSessionVar($key);
+    }
+
 
 
 }
