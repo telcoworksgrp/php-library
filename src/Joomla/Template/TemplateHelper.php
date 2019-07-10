b8427bc92253812aebc7c7b5348fbe292f715f53
David Plath <support@krealmwebservices.com.au>
2019-07-10 13:22:11 +1000

    Added helper class for working with Joomla template extensions

---
+++ src/Joomla/Template/TemplateHelper.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * =============================================================================
+ *
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ *
+ * =============================================================================
+ */
+
+namespace TCorp\Joomla\Template;
+
+
+
+/**
+ * Helper class for working with Joomla template extensions
+ */
+class TemplateHelper
+{
+
+}
