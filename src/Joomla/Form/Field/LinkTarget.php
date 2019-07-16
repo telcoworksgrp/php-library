986055aa31c0d4979ca0a6454ba11b1a871462fb
David Plath <webmaster@telecomcorp.com.au>
2019-07-16 13:53:44 +1000

    Added LinkTarget Joomla Form Field

---
+++ src/Joomla/Form/Field/LinkTarget.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * =============================================================================
+ *
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ *
+ * =============================================================================
+ */
+
+namespace TCorp\Joomla\Form\Field;
+
+use \Joomla\CMS\Factory;
+use \Joomla\CMS\HTML\HTMLHelper;
+use \Joomla\CMS\Form\FormHelper;
+
+
+// Load the parent FormField class
+FormHelper::loadFieldClass('list');
+
+
+/**
+ * Custom Form field for selecting a link target
+ */
+class LinkTarget extends \JFormFieldList
+{
+
+    protected $type = 'LinkTarget';
+
+
+    /**
+	 * Get a list of field options.
+	 * -------------------------------------------------------------------------
+	 * @return 	array 	An array of options (as HTML)
+	 */
+	protected function getOptions()
+	{
+        // Call the parent method
+        $options = parent::getOptions();
+
+		// Add field options to the result
+		$options[]= HTMLHelper::_('select.option', '_blank', 'Blank');
+        $options[]= HTMLHelper::_('select.option', '_self', 'Self');
+        $options[]= HTMLHelper::_('select.option', '_parent', 'Parent');
+        $options[]= HTMLHelper::_('select.option', '_top', 'Top');
+
+		// Return the resulting options (as html)
+		return $options;
+	}
+
+
+}
