abe4fe9565cea0e0ad5dadb086736c7d090f6828
David Plath <webmaster@telecomcorp.com.au>
2019-07-22 11:30:17 +1000

    Added a method for getting an instance of a table class from a component's back-end

--- src/Joomla/Component/ComponentHelper.php
+++ src/Joomla/Component/ComponentHelper.php
@@ -14,6 +14,7 @@ namespace TCorp\Joomla\Component;
 
 use \Joomla\CMS\Factory;
 use \Joomla\CMS\Component\ComponentHelper AS JoomlaComponentHelper;
+use \Joomla\CMS\Table\Table;
 
 
 /**
@@ -38,5 +39,40 @@ class ComponentHelper
 
         return JoomlaComponentHelper::getParams($name)->toObject();
     }
+
+
+    /**
+     * Get an instance of a table class from a component's back-end
+     * -------------------------------------------------------------------------
+     * @param  string   $component  The component to which the table class
+     *                              belongs ( eg: com_example )
+     * @param string    $type       The type (name) of the Table class to get
+     *                              an instance of
+     * @param  string   $prefix     An prefix for the table class name
+     * @param  array    $config     Array of config values for the Table object
+     *
+     * @return  Table|boolean   A Table object if found / false on failure.
+     */
+    public static function getAdminTable(string $component, string $type,
+        string $prefix = '', $config = [])
+    {
+        // Add the location of the component's back-end tables
+        Table::addIncludePath(JPATH_ADMINISTRATOR .
+            "/components/$component/tables");
+
+        // If no prefix has been given assume a concatenation of the component
+        // name (without "com_") and "Table".
+        if (empty($prefix)) {
+            $prefix = ucfirst(preg_replace('|^com_(.*)$|i',
+                '$1Table' , $component));
+        }
+
+        // Get an instance of the given table
+        $result = Table::getInstance($type, $prefix, $config);
+
+        // Return the final result
+        return $result;
+    }
+
 
 }
