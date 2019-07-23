15bb477e4f13fbf5fbeb2187d6c17763c6ee2677
David Plath <webmaster@telecomcorp.com.au>
2019-07-23 15:51:19 +1000

    Added model for working with a single message from the WebApi

---
+++ src/WebApi/Model/MessageModel.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * =============================================================================
+ *
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ *
+ * =============================================================================
+ */
+
+namespace TCorp\WebApi\Model;
+
+
+
+class MessageModel extends ItemModel
+{
+
+    protected static $endpoint = 'messages/';
+
+    protected static $properties = [
+        'id',
+       'from',
+       'replyto',
+       'to',
+       'cc',
+       'bcc',
+       'subject',
+       'body',
+       'created',
+       'note'
+    ];
+
+}
