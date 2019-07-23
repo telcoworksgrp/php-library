8dd688b3411187f2db5a42f2a43a1872b29a50e4
David Plath <webmaster@telecomcorp.com.au>
2019-07-23 15:19:04 +1000

    Added model for working with a single client from the WebApi

---
+++ src/WebApi/Model/Clients/ClientModel.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * =============================================================================
+ *
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ *
+ * =============================================================================
+ */
+
+namespace TCorp\WebApi\Model;
+
+use \TCorp\WebApi\Model\ItemModel;
+
+
+class ClientModel extends ItemModel
+{
+
+    protected static $endpoint = 'clients/';
+
+    protected static $properties = [
+        'id',
+    	'company',
+    	'alias',
+    	'intro',
+    	'description',
+    	'abn',
+    	'address',
+    	'suburb',
+    	'state',
+    	'postcode',
+    	'country',
+    	'latitude',
+    	'longitude',
+    	'phone',
+    	'fax',
+    	'mobile',
+    	'email',
+    	'website',
+    	'thumbnail',
+    	'image',
+    	'logo',
+    	'video',
+    	'published',
+    	'featured',
+    	'created',
+    	'note'
+    ];
+
+}
