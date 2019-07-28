e44f59f91a0dc4589ca3d65731fdebf08a92c8ea
David Plath <webmaster@telecomcorp.com.au>
2019-07-29 09:52:21 +1000

    The TCorp Amca helper now extends the KWS Acma Helper

--- src/Acma/AcmaHelper.php
+++ src/Acma/AcmaHelper.php
@@ -12,45 +12,10 @@
 
 namespace TCorp\Acma;
 
-use \League\Flysystem\Filesystem;
-use \League\Flysystem\ZipArchive\ZipArchiveAdapter;
-use \League\Csv\Reader AS CsvReader;
-
 
 /**
  * Helper class for working with ACMA
  */
-class AcmaHelper
+class AcmaHelper extends \KWS\Acma\AcmaHelper
 {
-
-
-    /**
-     * Download, extract, parse and then return ACMA's register of numbers from
-     * thier website. This method may take allot of time to complete. Caching
-     * the result is recommended. ACMA updated the register of numbers once
-     * every day at midnight.
-     * -------------------------------------------------------------------------
-     * @return string   A very large array of records parsed from the data.
-     */
-    public static function downloadRegisterOfNumbers()
-    {
-        // Download the ZIP file and save it to a tempory location
-        $filename = tempnam(sys_get_temp_dir(), '');
-        $url = 'https://www.thenumberingsystem.com.au/download/EnhancedFullDownload.zip';
-        file_put_contents($filename, file_get_contents($url));
-
-        // Extract the CSV file which contains the registar of numbers
-        $zipReader = new Filesystem(new ZipArchiveAdapter($filename));
-        $result = $zipReader->read('EnhancedFullDownload.csv');
-
-        // Parse the CSV data
-        $csvReader = CsvReader::createFromString($result);
-        $csvReader->setHeaderOffset(0);
-        $result = iterator_to_array($csvReader->getRecords());
-
-        // Return the result
-        return $result;
-    }
-
-
 }
