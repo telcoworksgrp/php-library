724d045eee90cb5fbb708ab146d231b8bc62b1f8
David Plath <webmaster@telecomcorp.com.au>
2019-07-29 09:56:55 +1000

    The TCorp Telephony helper now extends the KWS Telephony Helper

--- src/Telephony/TelephonyHelper.php
+++ src/Telephony/TelephonyHelper.php
@@ -13,30 +13,6 @@
 namespace TCorp\Telephony;
 
 
-class TelephonyHelper
+class TelephonyHelper extends \KWS\Telephony\TelephonyHelper
 {
-
-    /**
-     * Converts all letters in a given value to phone numbers
-     * -------------------------------------------------------------------------
-     * @param  string   $value  The value to digitise
-     *
-     * @return string   A digitised version of the value given
-     */
-    public static function digitise(string $value)
-    {
-        // Replace all phone letters with numbers
-        $result = preg_replace('|[abc]|i','2', $value);
-        $result = preg_replace('|[def]|i','3', $result);
-        $result = preg_replace('|[ghi]|i','4', $result);
-        $result = preg_replace('|[jkl]|i','5', $result);
-        $result = preg_replace('|[mno]|i','6', $result);
-        $result = preg_replace('|[pqrs]|i','7', $result);
-        $result = preg_replace('|[tuv]|i','8', $result);
-        $result = preg_replace('|[wxyz]|i','9', $result);
-
-        // Return the result
-        return $result;
-    }
-
-}    
+}
