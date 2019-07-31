7d15b5be79fdf0d1a8f8298ac14aeab0cb6eca15
David Plath <webmaster@telecomcorp.com.au>
2019-08-01 08:57:12 +1000

    TCorp Joomla sidebar helper now extend KWS Joomla sidebar helper

--- src/Joomla/Sidebar/SidebarHelper.php
+++ src/Joomla/Sidebar/SidebarHelper.php
@@ -19,48 +19,6 @@ use \Joomla\CMS\Language\Text;
 /**
  * Helper class for working with Joomla's admin sidebar
  */
-class SidebarHelper
+class SidebarHelper extends \KWS\Joomla\Sidebar\SidebarHelper;
 {
-
-    /**
-     * Add an item to the administration sidebar
-     * -------------------------------------------------------------------------
-     * @param string $caption   A caption for the item
-     * @param string $view      A view name to which the item will link to
-     * @param string $component The component to which the item will link to
-     *
-     * @return  void
-     */
-    public static function addSidebarItem(string $caption, string $view,
-        string $component) : void
-    {
-        // Prepare the sidebar item
-        $link    = "index.php?option=com_$component&view=$view";
-        $active  = Factory::getApplication()->input->get('view') == $view;
-
-        // Add the item to the sidebar
-        \JHtmlSidebar::addEntry(Text::_($caption), $link, $active);
-    }
-
-
-
-    /**
-     * Add a heading to the administration sidebar
-     * -------------------------------------------------------------------------
-     * @param string    $caption    A caption for the heading
-     * @param string    $tagName    A HTML tag name to enclose the title in
-     *
-     * @return  void
-     */
-    public static function addSidebarHeading(string $caption,
-        string $headingTag = 'h4') : void
-    {
-        // Prepare the sidebar heading
-        $heading = Text::_($caption);
-        $heading = "<$headingTag>$heading</$headingTag>";
-
-        // Add the heading to the sidebar
-        \JHtmlSidebar::addEntry($heading);
-    }
-
 }
