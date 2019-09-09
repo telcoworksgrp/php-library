207e415f06bbcef7efb0187d46b46f045a08e3bd
David Plath <webmaster@telecomcorp.com.au>
2019-09-10 08:37:57 +1000

    Added legacy class to manage configuration value

--- src/Legacy/Helper.php
+++ src/Legacy/Helper.php
@@ -25,119 +25,6 @@ class Helper
         'July','August','September','October','November','December');
 
 
-    /**
-     * A company name for the website
-     *
-     * @var string
-     */
-    public static $companyName = '';
-
-
-    /**
-     * The company's number and street name
-     *
-     * @var string
-     */
-    public static $companyAddress = '';
-
-
-    /**
-     * The suburb in which the company is located
-     *
-     * @var string
-     */
-    public static $companySuburb = '';
-
-
-    /**
-     * The state in which the company is located
-     *
-     * @var string
-     */
-    public static $companyState = '';
-
-
-    /**
-     * The postcode on which the company is located
-     *
-     * @var string
-     */
-    public static $companyPostcode = '';
-
-
-    /**
-     * THe country in which the company is located
-     *
-     * @var string
-     */
-    public static $companyCountry = '';
-
-
-    /**
-     * The company's contact phone number
-     *
-     * @var string
-     */
-    public static $companyPhone = '';
-
-
-    /**
-     * An main email address to which email notifications
-     * and other corraspondance will be sent
-     *
-     * @var string
-     */
-    public static $primaryEmail = '';
-
-
-    /**
-     * An additional email address to which email notifications
-     * and other corraspondance will be sent
-     *
-     * @var string
-     */
-    public static $secondaryEmail = '';
-
-
-    /**
-     * API key to use when calling the IpGeolocation API
-     *
-     * @var string
-     *
-     * @see https://ipgeolocation.io/
-     */
-    public static $ipGeoApiKey = '';
-
-
-    /**
-     * A Google ReCaptcha site key
-     *
-     * @var string
-     *
-     * @see https://www.google.com/recaptcha/intro/v3.html
-     */
-    public static $recaptchaSiteKey = '';
-
-
-    /**
-     * A Google ReCaptcha secret key to compliment the site key
-     *
-     * @var string
-     *
-     * @see https://www.google.com/recaptcha/intro/v3.html
-     */
-    public static $recaptchaSecret = '';
-
-
-    /**
-     * API key to use when calling the ABN lookup API
-     *
-     * @var string
-     *
-     * @see https://abr.business.gov.au/Tools/WebServices
-     */
-    public static $abnLookupGuid = '';
-
 
 
     /**
@@ -226,7 +113,7 @@ class Helper
      */
     public static function getReCaptchaHtml()
     {
-        return SecurityHelper::getReCaptchaHtml(static::$recaptchaSiteKey);
+        return SecurityHelper::getReCaptchaHtml(Config::$recaptchaSiteKey);
     }
 
 
@@ -288,7 +175,7 @@ class Helper
      */
     public static function redirectIfInvalidReCaptcha(string $redirectUrl) : void
     {
-        if (!SecurityHelper::checkReCaptcha(static::$recaptchaSecret)) {
+        if (!SecurityHelper::checkReCaptcha(Config::$recaptchaSecret)) {
             static::redirect($redirectUrl, false, 303);
         }
     }
@@ -519,7 +406,7 @@ class Helper
         $data = self::sendRequest($url, 'GET', array(
             'searchString'             => $abn,
             'includeHistoricalDetails' => 'Y',
-            'authenticationGuid'       => static::$abnLookupGuid
+            'authenticationGuid'       => Config::$abnLookupGuid
         ));
 
         // Parse the data returned by the API
