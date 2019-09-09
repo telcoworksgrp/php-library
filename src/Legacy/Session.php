c951f33a3f84535a1e26fe86759aecaf7cd33c02
David Plath <webmaster@telecomcorp.com.au>
2019-09-10 09:31:29 +1000

    Moved session related methods from legacy helper to the new legacy session class

--- src/Legacy/Session.php
+++ src/Legacy/Session.php
@@ -19,4 +19,80 @@ use \KWS\Registry\Registry;
  */
 class Session extends Registry
 {
+
+    /**
+     * Start a new session or resume and existing one
+     * -------------------------------------------------------------------------
+     * @return mixed
+     */
+    public function start()
+    {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        if (!isset($_SESSION['registry'])) {
+            $_SESSION['registry'] = new Registry();
+        }
+
+    }
+
+
+
+    /**
+     * Get a session value
+     * -------------------------------------------------------------------------
+     * @param  string   $key      Key/name of the session value to get
+     * @param  mixed    $default  Value to return if no value is found
+     *
+     * @return mixed
+     */
+    public function get(string $key, $default = null)
+    {
+        return $_SESSION['registry']->get($key, $default);
+    }
+
+
+
+    /**
+     * Set a session value
+     * -------------------------------------------------------------------------
+     * @param string    $key       Key/name of the session value to set
+     * @param mixed     $value     Value to set the session value to
+     *
+     * @return void
+     */
+    public function set(string $key, $value)
+    {
+        $_SESSION['registry']->set($key, $value);
+    }
+
+
+
+    /**
+     * Store the value of a request variable in a session var. If the request
+     * var doesn't exist then preserve the existing session var. If a session
+     * var with the given key doesn't exist then set a session var with the
+     * given key to a given default value.
+     * -------------------------------------------------------------------------
+     * @param string    $key        A key name for referancing the stored value
+     * @param string    $var        A GET/POST variable name
+     * @param string    $default    Default value if none can be found
+     *
+     * @return  mixed   The final value of session var
+     */
+    public function setFromRequest(string $key, string $var, $default = '')
+    {
+        if (isset($_REQUEST[$var])) {
+            $this->set($key, $_REQUEST[$var]);
+        } else {
+            if (!isset($_SESSION[$key])) {
+                $this->set($key, $default);
+            }
+        }
+
+        // Return the result
+        return $this->get($key);
+    }
+
 }
