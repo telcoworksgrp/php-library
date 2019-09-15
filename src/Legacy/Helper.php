69fdb691f1c41cd36063193a09f229a367c0b2ed
David Plath <webmaster@telecomcorp.com.au>
2019-09-16 08:36:57 +1000

    Moved consolidated legacy debugging methods to legacy helper

--- src/Legacy/Helper.php
+++ src/Legacy/Helper.php
@@ -546,4 +546,23 @@ class Helper
     {
         $_SESSION = [];
     }
+
+
+    /**
+     * Enable PHP error reporting
+     * -------------------------------------------------------------------------
+     * @param   bool    $enable True for full error reporting, False for none
+     *
+     * @return void
+     */
+    public static function enableErrorReporting(bool $enable = true) : void
+    {
+        if ($enable) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
+        } else {
+            error_reporting(E_NONE);
+            ini_set('display_errors', 0);
+        }
+    }
 }
