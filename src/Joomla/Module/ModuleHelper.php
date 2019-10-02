aa4f78356c61a287c9d9eaa193b9372f7e0b311e
David Plath <webmaster@telecomcorp.com.au>
2019-10-02 14:49:35 +1000

    Added base class for creating module helpers

---
+++ src/Joomla/Module/ModuleHelper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * =============================================================================
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * =============================================================================
+ */
+
+namespace TCorp\Joomla\Module;
+
+
+/**
+ * Base class for creating module helpers
+ */
+class ModuleHelper extends \KWS\Joomla\Module\ModuleHelper
+{
+}
