fce60c03e30456a1a66d149944ab65eacf41eeba
David Plath <webmaster@telecomcorp.com.au>
2019-10-15 11:47:46 +1100

    Moved WebApi client classes to the legacy namespace

---
+++ src/Legacy/WebApi/Client/Model/BaseModel.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * =============================================================================
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * =============================================================================
+ */
+
+namespace TCorp\Legacy\WebApi\Client\Model;
+
+use \TCorp\Legacy\WebApi\Client\BaseClient;
+
+
+/**
+ * Base class for creating models that abstract the web api
+ */
+class BaseModel
+{
+
+    /**
+     * An instance of a WebApi client for send/receiving
+     * data from the API
+     *
+     * @var \TCorp\Legacy\WebApi\BaseClient
+     */
+    protected $client = null;
+
+
+    /**
+     * Enpoint URL to send. Relative URLs are relative to the base Url
+     * defined in the WebApi Client
+     *
+     * @var string
+     */
+    protected static $endpoint = '';
+
+
+    /**
+     * Constructor for initialising new instances of this class
+     * -------------------------------------------------------------------------
+     * @param \TCorp\Legacy\WebApi\BaseClient    $apiClient    An WebApi client instance
+     */
+    public function __construct(BaseClient $client)
+    {
+        // Initialise some class properties
+        $this->client = $client ;
+    }
+
+}
---
+++ src/Legacy/WebApi/Client/Model/TutorialModel.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * =============================================================================
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * =============================================================================
+ */
+
+namespace TCorp\Legacy\WebApi\Client\Model;
+
+
+class TutorialModel extends BaseModel
+{
+
+    protected static $endpoint = 'tutorials';
+
+    protected static $properties = array(
+        'id',
+        'title',
+        'alias',
+        'intro',
+        'content',
+        'thumbnail',
+        'image',
+        'logo',
+        'video',
+        'created',
+        'updated',
+        'note'
+    );
+
+}
