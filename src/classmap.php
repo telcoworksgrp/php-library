c98a75271cc77827d0369cf19efb17a60aac0214
David Plath <webmaster@telecomcorp.com.au>
2019-10-22 10:15:54 +1100

    Changed classes that are extended from kws classes but have no additional methods/properties - to class aliases

---
+++ src/classmap.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * =============================================================================
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * =============================================================================
+ */
+
+\class_alias('TCorp\Acma\AcmaHelper',  \KWS\Acma\AcmaHelper::class);
+
+\class_alias('TCorp\Agent',      \KWS\Agent::class);
+\class_alias('TCorp\Arr',        \KWS\Arr::class);
+\class_alias('TCorp\Date',       \KWS\Date::class);
+\class_alias('TCorp\Debug',      \KWS\Debug::class);
+\class_alias('TCorp\File',       \KWS\File::class);
+\class_alias('TCorp\Firewall',   \KWS\Firewall::class);
+\class_alias('TCorp\Image',      \KWS\Image::class);
+\class_alias('TCorp\Input',      \KWS\Input::class);
+\class_alias('TCorp\Math',       \KWS\Math::class);
+\class_alias('TCorp\Request',    \KWS\Request::class);
+\class_alias('TCorp\Response',   \KWS\Response::class);
+\class_alias('TCorp\Session',    \KWS\Session::class);
+\class_alias('TCorp\Str',        \KWS\Str::class);
+\class_alias('TCorp\Uri',        \KWS\Uri::class);
+\class_alias('TCorp\Utils',      \KWS\Utils::class);
+\class_alias('TCorp\Validation', \KWS\Validation::class);
