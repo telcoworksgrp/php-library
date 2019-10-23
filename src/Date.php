ff2ba6e2a6abf4101a98acafb3ebaab6b0f60b52
David Plath <webmaster@telecomcorp.com.au>
2019-10-23 12:31:37 +1100

    Extended a number of core classes from the kwebservices/kws package

---
+++ src/Date.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * =============================================================================
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * =============================================================================
+ */
+
+namespace TCorp;
+
+
+/**
+ * Helper class for working with dates and times
+ */
+class Date extends \KWS\Date
+{
+}
