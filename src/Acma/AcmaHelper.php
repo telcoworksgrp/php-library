e78d35e7337ec899289c19da9049202f733e3ae5
David Plath <webmaster@telecomcorp.com.au>
2019-11-11 11:05:05 +1100

    Restored old acma helper class

---
+++ src/Acma/AcmaHelper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * =============================================================================
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * =============================================================================
+ */
+
+namespace TCorp\Acma;
+
+/**
+ * Helper class for working with ACMA
+ */
+class AcmaHelper extends \KWS\Acma\AcmaHelper
+{
+}
