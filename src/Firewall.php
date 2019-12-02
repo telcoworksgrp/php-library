4469cb5490c0b46ed503a7866afeda29b285e03c
David Plath <webmaster@telecomcorp.com.au>
2019-12-02 14:27:21 +1100

    Added method to Firewall class for blocking traffic

--- src/Firewall.php
+++ src/Firewall.php
@@ -16,4 +16,19 @@ namespace TCorp;
  */
 class Firewall
 {
+
+    /**
+     * Block the user by sending a 403 http response
+     * -------------------------------------------------------------------------
+     * @param  string  $message A HTTP response message
+     * @param  int     $code    A HTTP reponse code
+     *
+     * @return void
+     */
+    public function block(string $message = 'Forbidden', int $code = 403)
+    {
+        header("HTTP/1.0 $code $message");
+        die();
+    }
+
 }
