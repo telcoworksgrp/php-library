1cd9d8f2bc95bfcb1a2e3b1d0728d6128e99b341
David Plath <webmaster@telecomcorp.com.au>
2019-12-02 12:53:06 +1100

    Moved redirect method to Response class

--- src/Response.php
+++ src/Response.php
@@ -31,4 +31,34 @@ class Response
     }
 
 
+    /**
+     * Redirect the user's browser to another URL, optionally preserving the
+     * current URL parameters. If no url is given, the cuurent url will be
+     * used.This can be useful for returning to form submission pages but be
+     * careful not to create infinate redirects
+     * -------------------------------------------------------------------------
+     * @param string  $url              URL to redirect the user to
+     * @param bool    $preserveParams   Pass existing URL params to the redirect
+     * @param int     $statusCode        HTTP status code (usually 301 or 303)
+     *
+     * @return  void
+     */
+    public static function redirect(string $url = '', bool $preserveParams =
+        false, int $statusCode = 301) : void
+    {
+        // If no url is given, use the cuurent url. This can be useful for
+        // returning to form submission page
+        $url = (empty($url)) ? $_SERVER['REQUEST_URI'] : $url;
+
+        // Append the exitsing params if needed
+        if (($preserveParams) && (!empty($_SERVER['QUERY_STRING']))) {
+            $url = $url . ((strpos($url, '?')) ? '&' : '?') . $_SERVER['QUERY_STRING'];
+        }
+
+        // Redirect the user
+        header('Location: ' . $url, true, $statusCode);
+        exit();
+    }
+
+
 }
--- src/Utils.php
+++ src/Utils.php
@@ -22,51 +22,6 @@ use \ScssPhp\ScssPhp\Compiler AS SCSSCompiler;
 class Utils
 {
 
-    /**
-     * Disable browser caching of this request
-     * -------------------------------------------------------------------------
-     * @return void
-     */
-    public static function disableCache() : void
-    {
-        header("Cache-Control: max-age=0, no-cache, no-store, must-revalidate");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-        header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
-    }
-
-
-
-    /**
-     * Redirect the user's browser to another URL, optionally preserving the
-     * current URL parameters. If no url is given, the cuurent url will be
-     * used.This can be useful for returning to form submission pages but be
-     * careful not to create infinate redirects
-     * -------------------------------------------------------------------------
-     * @param string  $url              URL to redirect the user to
-     * @param bool    $preserveParams   Pass existing URL params to the redirect
-     * @param int     $statusCode        HTTP status code (usually 301 or 303)
-     *
-     * @return  void
-     */
-    public static function redirect(string $url = '', bool $preserveParams =
-        false, int $statusCode = 301) : void
-    {
-        // If no url is given, use the cuurent url. This can be useful for
-        // returning to form submission page
-        $url = (empty($url)) ? $_SERVER['REQUEST_URI'] : $url;
-
-        // Append the exitsing params if needed
-        if (($preserveParams) && (!empty($_SERVER['QUERY_STRING']))) {
-            $url = $url . ((strpos($url, '?')) ? '&' : '?') . $_SERVER['QUERY_STRING'];
-        }
-
-        // Redirect the user
-        header('Location: ' . $url, true, $statusCode);
-        exit();
-
-    }
-
 
     /**
      * Convert HTML to PDF document
