eb3457771711a3009236b9f7c3d898fdb4faba81
David Plath <webmaster@telecomcorp.com.au>
2019-12-03 13:38:56 +1100

    Added server side classes for the webapi

---
+++ src/WebApi/Server/Orm/Model.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * =============================================================================
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * =============================================================================
+ */
+
+namespace TCorp\WebApi\Server\Orm;
+
+
+
+class Model extends \Orm\Model
+{
+
+
+    public function hydrateFromInput()
+    {
+        $properties = array_keys($this->properties());
+
+        foreach ($properties as $name) {
+
+            $value = \Input::param($name, null);
+
+            if (!is_null($value)) {
+                $this->set($name, $value);
+            }
+        }
+
+    }
+
+}
