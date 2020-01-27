fe7546ec3961864268a430bdcd78eaec157b9c20
David Plath <support@krealmwebservices.com.au>
2020-01-28 08:43:02 +1000

    Updated legacy helper to use the legacy T3 class

--- src/Legacy/LegacyHelper.php
+++ src/Legacy/LegacyHelper.php
@@ -100,7 +100,7 @@ class LegacyHelper
 
 
     /**
-     * Get a list of numbers from the T3 API
+     * Proxy method for getting a list of numbers from the T3 API
      * -------------------------------------------------------------------------
      * @param  string   $prefix     Numbe prefix ('1300' or '1800')
      * @param  string   $type       Type of numbers to get ('FLASH' or 'LUCKYDIP')
@@ -117,38 +117,8 @@ class LegacyHelper
         $minPrice = 0, $maxPrice = 1000, $pageNo = 1, $pageSize = 500,
         $sortBy = 'PRICE', $direction = 'ASCENDING')
     {
-
-        // Compose an enpoint URL
-        $params                       = [];
-        $params['query']              = $prefix;
-        $params['numberTypes']        = 'SERVICE_NUMBER';
-        $params['serviceNumberTypes'] = $type;
-        $params['minPriceDollars']    = $minPrice;
-        $params['maxPriceDollars']    = $maxPrice;
-        $params['pageNum']            = $pageNo;
-        $params['pageSize']           = $pageSize;
-        $params['sortBy']             = $sortBy;
-        $params['sortDirection']      = $direction;
-
-
-        // Get the data from the API
-        $result = static::sendRequest(
-            'https://portal.tbill.live/numbers-service-impl/api/Activations',
-            'GET', $params, ['Content-type: application/json']);
-
-        // Decode JSON response
-        $result = json_decode($result);
-
-        // Add additional meta data
-        foreach($result as $number) {
-            $number->format1 = preg_replace('|^(\d{4})(\d{6})$|i', '$1 $2', $number->number);
-	        $number->format2 = preg_replace('|^(\d{4})(\d{3})(\d{3})$|i', '$1 $2 $3', $number->number);
-            $number->format3 = preg_replace('|^(\d{4})(\d{2})(\d{2})(\d{2})$|i', '$1 $2 $3 $4', $number->number);
-            $number->format4 = (!empty($number->word) ? $number->word : $number->format3);
-        }
-
-        // Return the result
-        return $result;
+        return Factory::getT3Client()->getNumbers($prefix, $type, $minPrice,
+            $maxPrice, $pageNo, $pageSize, $sortBy, $direction);
     }
 
 
