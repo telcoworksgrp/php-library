d1dfdb18e0bac111436273564e8ff6b2a408b95d
David Plath <support@krealmwebservices.com.au>
2020-01-28 08:28:17 +1000

    Added legacy application wrapper class

---
+++ src/Legacy/Application.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * =============================================================================
+ *
+ * @package     Telecom Corporation PHP Library
+ * @author      David Plath <webmaster@telecomcorp.com.au>
+ * @copyright   Copyright (C) 2019 Telecom Corporation. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ *
+ * =============================================================================
+ */
+
+namespace TCorp\Legacy;
+
+
+/**
+ * Class for implementing a Telecom Corp/Telcoworks Group legacy web application
+ */
+class Application
+{
+
+
+    /**
+     * Holds the global T3 client object
+     *
+     * @var \Legacy\T3
+     */
+    public $t3 = null;
+
+
+
+    /**
+     * Construtor method for initiailing new instances of this class
+     * -------------------------------------------------------------------------
+     * @return void
+     */
+    public function __constructor()
+    {
+        // Initialise some class properties
+        $this->t3 = Factory::getT3Client();
+    }
+
+
+}
